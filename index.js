3de7b940f84cfd74f0929cec9ae064a56c075800
Nemchick <cirillgoos@mail.ru>
2023-02-07 18:56:44 +0600

    upload

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import multer from 'multer';
+
 import mongoose from 'mongoose';
+
 import { registerValidation, loginValidation, postCreateValidation} from './validations.js';
+
 import checkAuth from './utils/checkAuth.js';
+
 import * as UserController from './controllers/UserController.js';
 import * as PostController from './controllers/PostController.js';
 
@@ -13,11 +18,28 @@ mongoose
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (__, _, cb)=>{
+        cb(null, 'uploads');
+    },
+    filename:(_, file, cb)=>{
+        cb(null, file.originalname);
+    },
+});
+
+const uploads = multer({storage});
+
 app.use(express.json());
 
 app.post('/auth/login',loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register );
 app.get('/auth/me', checkAuth, UserController.getMe );
+
+app.post('/upload',checkAuth, uploads.single('image'),(req, res)=>{
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+});
 
 app.get('/posts',PostController.getAll);
 app.get('/posts/:id',PostController.getOne);
